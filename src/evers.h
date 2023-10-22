61d8640533b0b2fcd0209f5a1b4fef42248e9598
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-10-22 10:01:26 +0200

    updating version to 2009/12/21

--- src/evers.h
+++ src/evers.h
@@ -44,7 +44,7 @@
 #define meCENTURY       "20"          /* Current century. Y2 complient :-) */
 #define meYEAR          "09"          /* Current year */
 #define meMONTH         "12"          /* Current month */
-#define meDAY           "20"          /* Day of the month */
+#define meDAY           "21"          /* Day of the month */
 
 /* Version information - Date of build */
 #define	meDATE          meYEAR "/" meMONTH "/" meDAY 
