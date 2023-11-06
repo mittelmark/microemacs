e1adecdcd56fc194e4e39f4b4a9dc732255b274e
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-11-06 17:40:35 +0100

    font converter script

---
+++ fonts/ttf2bdf.sh
@@ -0,0 +1,23 @@
+function ttf2bdf {
+    if [ -z $3 ]; then
+        echo -e "ttf2bdf - create X11 fonts from TTF fonts and make then available for X11-apps"
+        echo -e "Usage: ttf2bdf TYPENAME 'TTF-FILE' PREFIX"
+        echo -e "\n   Example: ttf2bdf 'Courier Prime.ttf' courier-prime\n"
+    else
+        for x in 7 10 12 14 16 18 20 24; do 
+            otf2bdf -t "$1" -p $x "$2" -o $3-${x}.bdf ; 
+        done
+        #cd ~/fonts
+        mkfontscale .
+        mkfontdir .
+        xset +fp `pwd`
+        xset fp rehash
+        echo "Available font names:"
+        echo "FILENAME                        FONTNAME"
+        grep -E "$3.+bdf" fonts.dir
+        echo "; place the following line into your USERNAME.emf file"
+        grep -E "$3.+bdf" fonts.dir | perl -ne 'm/freetype-(.+)-r-normal--([0-9]+)/ and $font=$1 and push(@sizes,$2); END { @sizes=sort(@sizes);print("change-font-type \"$font\" \"|",join("|",@sizes),"|\" $sizes[3]\n") }'
+    fi
+}
+
+ttf2bdf "$@"
