d79a18f84e5dee93c8c19e52b680a0995d02b725
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-11-06 17:48:17 +0100

    improving the font converter so that it as well show the emf code

--- fonts/courier-prime/ttf2bdf.sh
+++ fonts/courier-prime/ttf2bdf.sh
@@ -1,11 +1,11 @@
 function ttf2bdf {
-    if [ -z $2 ]; then
+    if [ -z $3 ]; then
         echo -e "ttf2bdf - create X11 fonts from TTF fonts and make then available for X11-apps"
-        echo -e "Usage: ttf2bdf 'TTF-FILE' PREFIX"
+        echo -e "Usage: ttf2bdf TYPENAME 'TTF-FILE' PREFIX"
         echo -e "\n   Example: ttf2bdf 'Courier Prime.ttf' courier-prime\n"
     else
         for x in 7 9 10 12 14 16 18 20 22 24 26; do 
-            otf2bdf -p $x "$1" -o $2-${x}.bdf ; 
+            otf2bdf -t "$1" -p $x "$2" -o $3-${x}.bdf ; 
         done
         #cd ~/fonts
         mkfontscale .
@@ -14,7 +14,7 @@ function ttf2bdf {
         xset fp rehash
         echo "Available font names:"
         echo "FILENAME                        FONTNAME"
-        grep -E "$2.+bdf" fonts.dir
+        grep -E "$3.+bdf" fonts.dir
     fi
 }
 
