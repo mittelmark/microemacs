ec0f0849587ad32a4202add4b381d3c14738ddcc
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-11-18 17:55:02 +0100

    continued me.smd 27percent

--- bin/me-tools.sh
+++ bin/me-tools.sh
@@ -3,10 +3,9 @@ function mds2html {
         echo "Usage: me.smd me-id folder"
         echo "        where me-id is soething like amicr000"
     else
-        perl -ne "/^!! me-id: $2/ and \$flag = 1 and next; \$flag and /^!!/ and last; \$flag and print" $1 | grep -Ev '^!' | perl -pe 's/</&lt;/g; s/([^ ]{2})>/$1&gt;/g' > $3/$2.md
+        perl -ne "/^!! me-id: $2/ and \$flag = 1 and next; \$flag and /^!!/ and last; \$flag and print" $1 | grep -Ev '^!' | perl -pe 's/</&lt;/g; s/([^ ]{2})>/$1&gt;/g' | perl -ne '/^(\||\$a)/ and next; /^ ([^ ].+)/ and print "$1  \n" and next; print;' > $3/$2.md
         perl -pe "s/__DATE__/`date +%Y-%m-%d`/" footer.html > foot.html
-        mkdoc htm/$2.md htm/$2.html --css me.css
-        cat header.html htm/$2.html foot.html > htm/$2.htm
+        mkdoc $3/$2.md $3/$2.html --css me.css --footer foot.html --header header.html
         #pandoc -f markdown-tex_math_dollars-raw_tex  htm/$2.md -o htm/$2.htm  --metadata title="MicroEmacs '09 - $2"  -s --css me.css -B header.html -A foot.html
     fi
 }           
