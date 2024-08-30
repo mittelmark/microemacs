c141558341d35b3af77e6df2a346a737dee01c9c
Detlef Groth <dgroth@github-noreply.com>
2024-08-30 15:30:51 +0200

    adding source code pro font installer

--- fonts/source-code-pro/install-source-code-pro-fonts.sh
+++ fonts/source-code-pro/install-source-code-pro-fonts.sh
@@ -1,13 +1,13 @@
+
 function source-code-pro-download {
     if  ! test -e source-code-pro-license.txt ; then
         wget https://github.com/adobe-fonts/source-code-pro/raw/release/LICENSE.md -O source-code-pro-license.txt
         wget https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Bold.ttf -O source-code-pro-bold.ttf
         wget https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Medium.ttf -O source-code-pro-medium.ttf        
-        wget https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Regular.ttf -O source-code-pro-regular.ttf                
         wget https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Semibold.ttf -O source-code-pro-semibold.ttf                        
     fi
 }
-function source-code-pro-cp-encode {
+function source-code-pro-reencode {
     for cp in cp1250 cp1251 cp1252
     do
         for w in regular medium semibold bold 
@@ -17,10 +17,39 @@ function source-code-pro-cp-encode {
     done
 }
 
+function in-fontpath {
+    if [ -z "$1" ]; then
+        DIR=`pwd`
+    else
+        DIR=$1
+    fi
+    if [ ! -d $DIR ]; then
+        echo "not a directory"
+    else  
+        FP=$(xset q | grep -A 1 "Font Path" | tail -n 1 | grep -o ${DIR})
+        if  [ "$FP" != "" ]; then
+            echo "true"
+        else  
+            echo "false"
+        fi        
+   fi
+}
+    
 function source-code-pro-index {
+    # indexing
     mkfontscale .
     mkfontdir .
-    xset +fp `pwd`
+    # fusing all scp-font names into one
+    sed -i -E 's/pro medium/pro/' fonts.dir 
+    sed -i -E 's/pro semibold/pro/' fonts.dir 
+    sed -i -E 's/pro semibold/pro/' fonts.scale
+    sed -i -E 's/pro medium/pro/' fonts.scale    
+    if [ $(in-fontpath) == "false" ] ; then
+        echo updated-fontpath
+        xset +fp `pwd`
+    fi
     xset fp rehash
 }
 
+source-code-pro-download
+source-code-pro-index
