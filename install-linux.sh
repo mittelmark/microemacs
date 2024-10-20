a9bf6b0fca8ce97a4169fcbf04dd31118ab15d30
Detlef Groth <dgroth@github-noreply.com>
2024-10-20 06:08:09 +0200

    update for install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,45 +1,88 @@
 #!/bin/bash
 
-function install-jme {
-    # Define the URL to download the dummy.sh script
-    DOWNLOAD_URL="https://github.com/mittelmark/microemacs/releases/download/v09.12.21/Jasspa_MicroEmacs-x86_64.AppImage"
-    DOWNLOAD_URL="https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta1/Jasspa_MicroEmacs_09-x86_64.AppImage"
-    # Create the ~/bin directory if it doesn't exist
-    if [ ! -d ~/bin ]; then
-        mkdir -p ~/bin
+BASEURL="https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta2/"
+KERNEL=`uname -r | grep -Eo '^[0-9]'`
+if [[ "`uname -r | grep -E 'fc[0-9]'`" != "" ]]; then
+    ## fedora
+    if [[ $KERNEL -eq 5 ]]; then    
+       MECB="linux-5-fedora-30-microemacs-091224b2-mecb"
+       MEWB="linux-5-fedora-30-microemacs-091224b2-mewb"   
+    elif [[ $KERNEL -eq 6 ]]; then    
+       MECB="linux-6-fedora-40-microemacs-091224b2-mecb"
+       MEWB="linux-6-fedora-40-microemacs-091224b2-mewb"   
+    else
+        echo "Error: Kernel $KERNEL not supported!"    
+        
     fi
-
-    # Check if ~/bin is already in the PATH
-    if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
-        # If it's not in the PATH, add it to ~/.bashrc
-        echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
-        # Update the PATH for the current session
-        export PATH="$HOME/bin:$PATH"
+else
+    if [[ $KERNEL -eq 5 ]]; then
+        MECB="linux-5-ubuntu-20-microemacs-091224b2-mecb"
+        MEWB="linux-5-ubuntu-20-microemacs-091224b2-mewb"
+    elif [[ $KERNEL -eq 6 ]]; then
+        MECB="linux-6-ubuntu-22-microemacs-091224b2-mecb"
+        MEWB="linux-6-ubuntu-22-microemacs-091224b2-mewb"
+    else
+        echo "Error: Kernel $KERNEL not supported!"    
     fi
-
-    # Download the dummy.sh script from the internet
-    wget -O ~/bin/jme "$DOWNLOAD_URL"
-
-    # Make the script executable
-    chmod 755 ~/bin/jme
-
-    echo "Installation complete."
-}
-
-## install fonts
-
+fi
 
 function install-fonts {
-    if [ ! -d ~/.local/share/jasspa/fonts ]; then
+    if [[ ! -d ~/.local/share/jasspa/fonts ]]; then
         mkdir -p ~/.local/share/jasspa/fonts
+        wget -O ttf-files.zip https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta1/ttf-fonts.zip
+        unzip -j -d ~/.local/share/jasspa/fonts ttf-files.zip
     fi  
-    wget -O ttf-files.zip https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta1/ttf-fonts.zip
-    unzip -j -d ~/.local/share/jasspa/fonts ttf-files.zip
     FP=`xset q | grep -A1 'Font Path' | grep 'local/share/jasspa/fonts'`
-    if [ "$FP" == "" ]; then
+    if [[ "$FP" == "" ]]; then
         xset +fp ~/.local/share/jasspa/fonts
         xset fp rehash
     else   
         xset fp rehash
     fi
 }
+
+function install-me {
+    if [[ ! -f ~/.local/bin/mecb ]]; then
+        if [ ! -d ~/.local/bin ]; then
+            mkdir -p ~/.local/bin
+        fi
+
+        # Check if ~/bin is already in the PATH
+        if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+            # If it's not in the PATH, add it to ~/.bashrc
+            echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+            # Update the PATH for the current session
+            export PATH="$HOME/.local/bin:$PATH"
+        fi
+
+        # Download the dummy.sh script from the internet
+        URL=${BASEURL}${MECB}
+        wget -P /tmp/ "${URL}.zip"
+        unzip -p "/tmp/${MECB}.zip" $MECB/bin/mecb > ~/.local/bin/mecb
+        URL=${BASEURL}${MEWB}
+        wget -P /tmp/ "${URL}.zip"
+        unzip -p "/tmp/${MEWB}.zip" $MEWB/bin/mewb > ~/.local/bin/mewb
+        # Make the script executable
+        chmod 755 ~/.local/bin/me?b
+        wget https://raw.githubusercontent.com/mittelmark/microemacs/refs/heads/master/install-linux.sh -O ~/.local/bin/me
+        chmod 755 ~/.local/bin/me
+        echo "Installation complete."
+    fi    
+}
+
+install-me
+install-fonts
+
+echo $0
+if [[ $1 == "-w" ]]; then
+    shift
+    echo here2   
+    mewb "$@"
+elif [[ $1 == "-n" ]]; then
+    shift
+    echo here3   
+    mecb "$@"
+else
+    echo here4
+    mecb "$@"
+fi
