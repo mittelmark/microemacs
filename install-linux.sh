5a3caaeff242b323640a10a5e946f5ff45908827
Detlef Groth <dgroth@github-noreply.com>
2024-10-20 06:55:17 +0200

    install-linux switching wget to curl

--- install-linux.sh
+++ install-linux.sh
@@ -29,8 +29,8 @@ fi
 function install-fonts {
     if [[ ! -d ~/.local/share/jasspa/fonts ]]; then
         mkdir -p ~/.local/share/jasspa/fonts
-        wget -O ttf-files.zip https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta1/ttf-fonts.zip
-        unzip -j -d ~/.local/share/jasspa/fonts ttf-files.zip
+        curl -fsSL https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta1/ttf-fonts.zip --output /tmp/ttf-files.zip 
+        unzip -j -d ~/.local/share/jasspa/fonts /tmp/ttf-files.zip
     fi  
     FP=`xset q | grep -A1 'Font Path' | grep 'local/share/jasspa/fonts'`
     if [[ "$FP" == "" ]]; then
@@ -58,25 +58,26 @@ function install-me {
         # Download the dummy.sh script from the internet
         URL=${BASEURL}${MECB}
         if [[ ! -f "/tmp/${MECB}.zip" ]]; then
-            wget -P /tmp/ "${URL}.zip"
+            curl  -fsSL "${URL}.zip" --output /tmp/${MECB}.zip 
         fi
         unzip -p "/tmp/${MECB}.zip" $MECB/bin/mecb > ~/.local/bin/mecb
         unzip -p "/tmp/${MECB}.zip" $MECB/bin/mecu > ~/.local/bin/mecu
         URL=${BASEURL}${MEWB}
         if [[ ! -f "/tmp/${MEWB}.zip" ]]; then
-            wget -P /tmp/ "${URL}.zip"
+            curl -fsSL "${URL}.zip" --output /tmp/${MEWB}.zip 
         fi
         unzip -p "/tmp/${MEWB}.zip" $MEWB/bin/mewb > ~/.local/bin/mewb
         # Make the script executable
         chmod 755 ~/.local/bin/me?b
         if [[ ! -f ~/.local/bin/me ]]; then
-            wget https://raw.githubusercontent.com/mittelmark/microemacs/refs/heads/master/install-linux.sh -O ~/.local/bin/me
+            curl -fsSL https://raw.githubusercontent.com/mittelmark/microemacs/refs/heads/master/install-linux.sh --output ~/.local/bin/me 
             chmod 755 ~/.local/bin/me
         fi
         echo "Installation complete."
     fi    
 }
 
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mittelmark/microemacs/refs/heads/master/install-linux.sh)"
 install-me
 install-fonts
 
