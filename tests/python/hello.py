7ba025990cb897d98946238b758673443e8f3f55
Detlef Groth <dgroth@ubuntu22.vm>
2025-05-25 08:12:55 +0200

    c3 file support added, file-lint regex for lint lines changed

---
+++ tests/python/hello.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: ISO-8859-15 -*-
+"""Modulename
+
+Usage: {0} ?-h,-?,--help?
+
+Arguments:
+"""
+
+__author__ = "first last"
+__version__ = "0.1"
+import sys
+#import os
+#import re
+
+
+def help(argv):
+    print(__doc__.format(argv[1]))
+
+
+def usage(argv):
+    print(f"Usage: {argv[0]} args")
+
+
+def main(argv):
+    if (len(argv)) == 1:
+        usage(argv)
+
+    elif "-h" in argv or "--help" in argv:
+        help(argv)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
