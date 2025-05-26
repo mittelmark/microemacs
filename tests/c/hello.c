001afac75758ec76d920172311b734e8fd6746bc
Detlef Groth <dgroth@github-noreply.com>
2025-05-26 12:03:56 +0200

    adding support for c-bool hilghlights, and shortening c template

---
+++ tests/c/hello.c
@@ -0,0 +1,17 @@
+/* -*- C -*- ****************************************************************
+ *
+ *  Copyright (c) 2025 Detlef Groth.
+ * 
+ *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main (char * argv[]) {
+    bool x = true ;
+    printf("Hello World!\n");
+    printf("%d\n",x);
+    return(0);
+}
+
