789836fe45fafb0c409cc7b961bccff11b536410
Detlef Groth <dgroth@github-noreply.com>
2025-05-30 10:15:55 +0200

    filtools.emf now working as well for c3c

--- tests/c/hello.c
+++ tests/c/hello.c
@@ -1,15 +1,15 @@
 /* -*- C -*- ****************************************************************
  *
  *  Copyright (c) 2025 Detlef Groth.
- * 
+ *
  *
  ****************************************************************************/
 
 #include <stdio.h>
 #include <stdbool.h>
 
-int main (char * argv[]) {
-    ool x = true ;
+int main (int argc, char * argv[]) {
+    bool x = true ;
     printf("Hello World!\n");
     printf("%d\n",x);
     return(0);
