b1dbedf9b27aac33c21f5b40ae18193624a150c4
Detlef Groth <>
2025-08-12 12:22:23 +0200

    adding indentation to julia file hook

--- tests/julia/hello.jl
+++ tests/julia/hello.jl
@@ -1,3 +1,17 @@
 #!/usr/bin/env julia
+## this a comment
+#= And here comes a multiline comment
+   which can be written over several lines 
+   it is ended with an equal sign anfd and
+   hash sign like here: =#
 println("hello world")
 
+## loop example
+for i in 1:10
+    if i % 3 == 0
+        continue
+    end
+    println(i)
+end 
+
+    
