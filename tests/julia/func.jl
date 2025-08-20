d58e733e43d36476856b38c02cf46c47216755f5
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-08-20 15:47:59 +0200

    support for file-tools for julia code

--- tests/julia/func.jl
+++ tests/julia/func.jl
@@ -2,7 +2,7 @@
 function sphere_vol(r)
     # julia allows Unicode names (in UTF-8 encoding)
     # so either "pi" or the symbol ? can be used
-    return 4/3*pi*r^3
+    return 4 / 3 * pi * r^3
 end
 
 # functions can also be defined more succinctly
@@ -12,22 +12,22 @@ quadratic(a, sqr_term, b) = (-b + sqr_term) / 2a
 function quadratic2(a::Float64, b::Float64, c::Float64)
     # unlike other languages 2a is equivalent to 2*a
     # a^2 is used instead of a**2 or pow(a,2)
-    sqr_term = sqrt(b^2-4a*c)
+    sqr_term = sqrt(b^2 - 4a * c)
     r1 = quadratic(a, sqr_term, b)
     r2 = quadratic(a, -sqr_term, b)
     # multiple values can be returned from a function using tuples
     # if the return keyword is omitted, the last term is returned
-    r1, r2
+    return r1, r2
 end
 
 vol = sphere_vol(3)
 # @printf allows number formatting but does not automatically append the \n to statements, see below
 using Printf
-@printf "volume = %0.3f\n" vol 
+@printf "volume = %0.3f\n" vol
 #> volume = 113.097
 
 quad1, quad2 = quadratic2(2.0, -2.0, -12.0)
 println("result 1: ", quad1)
 
 # hello fffffffffffffffffffffffffffffffff                                 #
-# ffffffffffffffffffffffffffffffffffff fffffff fff fgggggggggggg            # 
+# ffffffffffffffffffffffffffffffffffff fffffff fff fgggggggggggg            #
