45ed586cc42a90e7ad2ecac147aef90cc0f19f4b
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-08-21 09:36:16 +0200

    adding support for formatting and executing Octave code from ME

--- tests/octave/hello.m
+++ tests/octave/hello.m
@@ -1,15 +1,16 @@
 #!/usr/bin/octave -W
+
 #
-#  Copyright (c) 2025, Detlef Groth
-#  License       :
-#  Date          : $Date$
-#  Author        : $Author$
-#  Created By    : Detlef Groth
-#  Created       : Thu Jul 24 06:25:18 2025
-#  Last Modified : <250812.1316>
+# Copyright (c) 2025, Detlef Groth
+# License:
+# Date:$Date$
+# Author:$Author$
+# Created By:Detlef Groth
+# Created:Thu Jul 24 06:25:18 2025
+# Last Modified: < 250812.1316 >
 #
-#  Description	 : Just test code for MicroEmacs
-#                  verifying the Octave mode of ME
+# Description:Just test code for MicroEmacs
+# verifying the Octave mode of ME
 #
 ###################################################
 
@@ -20,13 +21,17 @@ wrap over several lines.
 %}
 
 ### a greeting function
-function greet        
+
+function greet
+
     for i = 1:5
         disp('Hello Octave World!')
     endfor
+
 endfunction
 
 ### a test function
+
 function test(msg)
     disp(msg)
 endfunction
@@ -34,8 +39,9 @@ endfunction
 test('testing ...')
 greet()
 
-fprintf('4 * 5 is %d\n',4*5)
-x = int8(rand()*20)
+fprintf('4 * 5 is %d\n', 4 * 5)
+x = int8(rand() * 20)
+
 if x < 5
     disp('x is small')
 elseif x < 10
@@ -44,4 +50,4 @@ else
     disp('x is large')
 end
 
-fprintf('x was %d\n',x)
+fprintf('x was %d\n', x)
---
+++ tests/octave/hw.m
@@ -0,0 +1,6 @@
+#!/usr/bin/octave -W
+
+printf("Hello Octave World!\n")
+display(x)
+
+printf("Hi\n")
