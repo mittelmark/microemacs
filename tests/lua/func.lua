cc584655328a7b308cb3a4e2cf3c311a12d6b9f2
Detlef Groth <>
2025-08-23 09:49:47 +0200

    updating changelog in me.smd

---
+++ tests/lua/func.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env lua
+--------------------------------------------
+--
+-- Date          : $Date$
+-- Author        : $Author$
+-- Created By    : dgroth
+-- Created       : Tue Aug 19 19:03:53 2025
+-- Last Modified : <250819.1905>
+--
+-- Description	 :
+--
+-- History       :
+--	
+--------------------------------------------
+--
+--  Copyright (c) 2025 dgroth.
+-- 
+--  License: See file LICENSE in this folder
+--
+--------------------------------------------
+
+
+function hi ()
+    print("Hi")
+end
+
+hi
---
+++ tests/lua/new.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/env lua
+--------------------------------------------
+--
+-- Date          : $Date$
+-- Author        : $Author$
+-- Created By    : dgroth
+-- Created       : Tue Aug 19 18:38:03 2025
+-- Last Modified : <250820.0654>
+--
+-- Description	 :
+--
+--  History      :
+--	
+--------------------------------------------
+--
+--  Copyright (c) 2025 dgroth.
+-- 
+--  License: See file LICENSE in this folder
+--
+--------------------------------------------
+
+function hello ()
+    print("Hello")
+end
+
+function test2 ()
+   print("Test 2") 
+end
+hello
+test2
