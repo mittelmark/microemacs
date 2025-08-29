e26b380405516bc63f2c5424b52484fb3cf898de
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-08-29 12:44:50 +0200

    adding CCc as hilight for abbrev-list, fixing a few c abbrevs

--- tests/c/hello.c
+++ tests/c/hello.c
@@ -8,9 +8,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main (int argc, char * argv[]) {
+int main (int argc, char * argv[]) 
+{
+    int x = 1;
+    if (x<3) 
+    {
+        printf("Hi"):
+    }
     bool x = true ;
     printf("Hello World!\n");
     printf("%d\n",x);
     return(0);
 }
+
+struct x
+{
+    int x = 3
+};
+
