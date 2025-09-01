8c3b3a2b23ccf261026aeab9a77605ac6e68a8ff
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-09-01 07:44:44 +0200

    install-fonts.sh ready to use; adding folding to shell code

--- install-fonts.sh
+++ install-fonts.sh
@@ -5,19 +5,38 @@ function install-fonts {
     fi  
     ### link the required ttf files
     find  /usr/ -iregex \
-      ".*\\(Mono\\|Mono-?Bold\\|Mono-?Regular\\|Code-Regular\\|Code-Bold\\).ttf"  \
+      ".*\\(Mono\\|Mono-?Bold\\|Mono-?Regular\\|Code-Regular\\|Code-Bold\\).ttf" 2>/dev/null \
       | xargs ln -sf -t ~/.local/share/fonts/
     #mkdir -p ~/.local/share/jasspa/fonts
     #    curl -fsSL https://github.com/mittelmark/microemacs/releases/download/v09.12.24.beta1/ttf-fonts.zip --output /tmp/ttf-files.zip 
     #    unzip -j -q -d ~/.local/share/fonts /tmp/ttf-files.zip
-    [ -f "$HOME/.local/share/fonts/DejaVuSansMono.ttf" && -f "$HOME/.local/share/fonts/courier-prime-medium.ttf" ] && install-dejavu-mono
+    if [[ -f "$HOME/.local/share/fonts/DejaVuSansMono.ttf" || -f "$HOME/.local/share/fonts/dejavu-sans-mono-medium.ttf" ]]; then
+        echo "Dejavu fonts are already installed!"
+    else    
+        install-dejavu-mono 
+    fi  
+    if [[ -f "$HOME/.local/share/fonts/CourierPrime-Regular.ttf" || -f "$HOME/.local/share/fonts/courier-prime-medium.ttf" ]]; then
+        echo "Courier Prime fonts are already installed!"
+    else
+         install-courier-prime        
+    fi
+    if [[ -f "$HOME/.local/share/fonts/SourceCodePro-Regular.ttf" || -f "$HOME/.local/share/fonts/source-code-pro-medium.ttf" ]]; then
+        echo "Source Code Pro fonts are already installed!"
+    else 
+        install-source-code-pro
+    fi
+    if [[ -f "$HOME/.local/share/fonts/UbuntuMono-Regular.ttf" || -f "$HOME/.local/share/fonts/ubuntu-mono-medium.ttf" ]]; then
+        echo "Ubuntu Mono fonts are already installed!"
+    else 
+        install-ubuntu-mono
+    fi
     mkfontscale ~/.local/share/fonts
     mkfontdir ~/.local/share/fonts
 }
 
 function update-font-path {
     ## check if the local folder is already in the fontpath
-    FP=`xset q | grep -A1 'Font Path' | grep '~/.local/share/fonts'`
+    FP=`xset q | grep -A1 'Font Path' | grep '.local/share/fonts'`
     ## current session
     if [ "$FP" = "" ]; then
         ## if not add it for the current X-session
@@ -27,20 +46,24 @@ function update-font-path {
             ## AntiX-Linux
             echo '(sleep 3 && xset +fp ~/.local/share/fonts && xset fp rehash) &' >> ~/.desktop-session/config
         fi  
-        if [ -f ~/.config/autostart ]; 
+        if [ -f ~/.config/autostart ];  then
             ## other desktop managers Cinnamon
             ## create X11-Font-Load desktop file in config/autostart 
             ## create 
             echo "#!/bin/sh" > ~/.local/bin/ttf-fonts.sh
             echo '(sleep 3 && xset +fp ~/.local/share/fonts && xset fp rehash) &' >> ~/.local/bin/ttf-fonts.sh
-            
-            
-        then
+            echo "[Desktop Entry]" > ~/.config/autostart/ttf-fonts.desktop
+            echo "Type=Application" >> ~/.config/autostart/ttf-fonts.desktop
+            echo "Exec=sh ~/.local/bin/ttf-fonts.sh" >> ~/.config/autostart/ttf-fonts.desktop    
+            echo "Hidden=false" >> ~/.config/autostart/ttf-fonts-desktop 
+            echo "NoDisplay=true" >> ~/.config/autostart/ttf-fonts.desktop
+            echo "X-GNOME-Autostart-enabled=true" >> ~/.config/autostart/ttf-fonts.desktop
+            echo "Name=TTF-Fonts" >> ~/.config/autostart/ttf-fonts.desktop
+        fi
 
     else   
         xset fp rehash
     fi
-    FP=
 }
 ## link into ~/.local/share/fonts
 function install-dejavu-mono {
@@ -48,16 +71,21 @@ function install-dejavu-mono {
     wget https://github.com/go-fonts/dejavu/raw/main/LICENSE-DejaVu -O ~/.local/share/fonts/dejavu-sans-mono-license.txt
     wget https://github.com/go-fonts/dejavu/raw/main/dejavusansmono/DejaVuSansMono.ttf -O ~/.local/share/fonts/dejavu-sans-mono-medium.ttf
     wget https://github.com/go-fonts/dejavu/raw/main/dejavusansmonobold/DejaVuSansMono-Bold.ttf -O ~/.local/share/fonts/dejavu-sans-mono-bold.ttf
-
 }
 
 function install-source-code-pro {
     echo "install course-code-pro into ~/.local/share/fonts"    
+    wget https://github.com/adobe-fonts/source-code-pro/raw/release/LICENSE.md -O ~/.local/share/fonts/source-code-pro-license.txt
+    wget https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Bold.ttf -O ~/.local/share/fonts/source-code-pro-bold.ttf
+    wget https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Regular.ttf -O ~/.local/share/fonts/source-code-pro-medium.ttf
 
 }
 
 function install-ubuntu-mono {
     echo "install ubuntu-mono into ~/.local/share/fonts"
+    wget https://github.com/braver/programmingfonts/raw/gh-pages/fonts/resources/ubuntu/ubuntu-bold.ttf -O ~/.local/share/fonts/ubuntu-mono-bold.ttf
+    wget https://github.com/braver/programmingfonts/raw/gh-pages/fonts/resources/ubuntu/ubuntu.ttf    -O ~/.local/share/fonts/ubuntu-mono-medium.ttf    
+    wget https://github.com/braver/programmingfonts/raw/gh-pages/fonts/resources/ubuntu/license.txt -O ~/.local/share/fonts/ubuntu-license.txt
 }
 
 function install-courier-prime() {
@@ -68,4 +96,4 @@ function install-courier-prime() {
 }
 
 install-fonts
-update-fontpath
+update-font-path
