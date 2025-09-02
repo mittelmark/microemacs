b0fab0c9d84864a9942218fca0b0a57ba74fa067
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-09-02 17:27:27 +0200

    check for cygwin and AlmaLinux 10 version

--- install-unix.sh
+++ install-unix.sh
@@ -18,7 +18,7 @@ if [ $OS = "MSYS" ]; then
     EXE=".exe"
 elif [ $OS = "CYGWIN" ]; then
     ## TODO: check version 3.3, 3.5, 3.6
-    CYGVERSION=`cygcheck -cd cygwin | grep cygwin | sed -E 's/.+(3.[0-9]).+/\1/'`
+    CYGVERSION=`cygcheck -cd cygwin | grep cygwin | sed -E 's/.+ (3.[0-9]).+/\1/'`
     if [ "$CYGVERSION" = "3.4" ]; then
         CYGVERSION="3.3"
     fi
