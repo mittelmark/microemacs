abbd80920b0102fa27dfe7f2d4bba406a2b1d236
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-09-11 08:16:09 +0200

    fixing syntax hilighting in Powershell and item-list issue with functions

--- tests/ps1/hello.ps1
+++ tests/ps1/hello.ps1
@@ -1,3 +1,21 @@
 ## -!- ps1 -!-
+<#
+Here follows a multiline comment
+which can have many lines
+#>
+
 Write-Host 'Hello, World!'
 'Hello, World!' | Write-Host
+
+function Get-Fibonacci ($n) {
+    if ($n -lt 2) {
+        $n
+    }
+    else {
+        (Get-Fibonacci ($n - 1)) + (Get-Fibonacci ($n - 2))
+    }
+}
+
+# TODO: Check - If this works below
+
+Get-Fibonacci 3 | Write-Host 
