0c4655204b9d9d79f1ae6ae718e8153fc5f5b053
Detlef Groth <detlef.groth@uni-potsdam.de>
2025-10-09 09:19:49 +0200

    starting nim programming language support

---
+++ tests/nim/hello.nim
@@ -0,0 +1,17 @@
+#!/usr/bin/env nim r
+# comment
+
+#[
+This is a multiline comment
+which can span over several lines
+]#
+
+echo "Hello Nim World!"
+
+var x = 2
+echo x
+if x > 3:
+  echo "x is large"
+else:
+  echo "x is small"
+echo y
