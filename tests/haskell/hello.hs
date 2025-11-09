74712f7a634ada03a9538859780dd839f9def4ea
Detlef Groth (D7480) <dgroth>
2025-11-09 22:55:00 +0000

    adding support for function defintions with :: operator for item-list and folding

--- tests/haskell/hello.hs
+++ tests/haskell/hello.hs
@@ -3,6 +3,9 @@ This is a comment
 which can span multiple lines
 -}
 
+add                     :: Integer -> Integer -> Integer
+add x y                 =  x + y
+
 factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = do
@@ -14,3 +17,4 @@ main :: IO ()
 main = do
   print ("Hello Haskell World!")
   print (factorial 6)
+  print (add 2 3)
