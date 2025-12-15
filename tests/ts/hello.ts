982ccfe9bc73f4212906a62619224d8e5f4c125d
Detlef Groth (D7480) <dgroth>
2025-12-15 18:54:50 +0000

    initial typescript support

---
+++ tests/ts/hello.ts
@@ -0,0 +1,19 @@
+/* -*- typescript -*-
+     Copyright 2025 University of Potsdam, Germany
+     All Rights Reserved
+     System        : HELLO_TS :
+     Object Name   : $RCS_FILE$
+     Revision      : $REVISION$
+     Date          : Mon Dec 15 18:35:42 2025
+     Created By    : Detlef Groth, University of Potsdam, Germany
+     Created       : Mon Dec 15 18:35:42 2025
+
+     Last Modified : <251215.1852>
+     Description
+     Notes
+*/
+console.log("Hello Typescript!")
+let x = 3, number;
+if (x < 3) {
+  console.log("x is small");
+}
