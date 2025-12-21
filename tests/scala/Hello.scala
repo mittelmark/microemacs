da6a2cedc0f5e4b91f8e58a20d8f005a3a2013f2
Detlef Groth (D7480) <dgroth>
2025-12-21 14:42:32 +0000

    more docu fixes fro scala and typescript

--- tests/scala/Hello.scala
+++ tests/scala/Hello.scala
@@ -1,8 +1,13 @@
 object Hello extends App {
   var x = 1;
+  def test (): Unit = {
+    println("Hello some test function!")
+  }
+
   if (args.size == 0) {
     println("Hello Scala World!")
   } else {
     println("Hello, "+args(0)+"!")
   }
+  test()
 }
