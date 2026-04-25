350524036f27ee13bf9296a958b5ad3ae609a373
Detlef Groth <groth@antix23.vm>
2026-04-25 12:23:21 +0200

    fixing direct mingw build bfs

--- bfs/uxdir.c
+++ bfs/uxdir.c
@@ -116,11 +116,12 @@ create_dir (char *dirname)
 
     /* File system call to create a directory. There is a difference between
      * UNIX and other systems. Differentiate the systems based on the macro
-     * definitions. */
-#ifndef _WIN32
-    status = mkdir (dirname, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+     * definitions.
+     * Note: MSYS2/MinGW uses 2-arg mkdir like Unix. Only old Windows needs 1 arg. */
+#if defined(__MINGW32__) || defined(__MINGW64__) || defined(_WIN32)
+    status = mkdir (dirname, 0755);
 #else
-    status = mkdir (dirname);    
+    status = mkdir (dirname, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 #endif
     return status;
 }
