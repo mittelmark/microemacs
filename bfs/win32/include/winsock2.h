fd8ff23cf5764b19aae2d7f1c8c7c3808929b57e
Detlef Groth <groth@antix23.vm>
2026-04-27 15:54:49 +0200

    adding winsock2 for ming/msys builds, fixing select

--- bfs/win32/include/winsock2.h
+++ bfs/win32/include/winsock2.h
@@ -1,30 +1,24 @@
 /*
- * winsock2.h - Compatibility header to fix select() conflict
+ * winsock2.h - Fix select() conflict for MSYS2/MinGW
  *
- * MSYS2 winsock2.h declares select() with TIMEVAL *, but sys/select.h declares it
- * with struct timeval *. This header resolves the conflict.
+ * The problem: sys/select.h declares select() with struct timeval *,
+ * but winsock2.h declares it with const TIMEVAL *.
+ * This header fixes the conflict by undefining select before
+ * including the system winsock2.h.
  *
- * Copyright (C) 2025 JASSPA (www.jasspa.com)
- * This is free software; you can redistribute it and/or modify
- * under the terms of the GNU General Public License.
+ * Copyright (C) 2025 JASSPA
  */
-
 #ifndef _WINSOCK2_H
 #define _WINSOCK2_H
 
 #ifdef _WIN32
-
-/*
- * Fix the select() conflict - undef the POSIX select before including winsock2.h.
- * This prevents "conflicting types for 'select'" error.
- */
+/* Undef the POSIX select to prevent conflict - winsock2.h will provide its own */
 #ifdef select
 #undef select
 #endif
 
+/* Include the system winsock2.h */
 #include <winsock2.h>
-#include <ws2tcpip.h>
 
 #endif /* _WIN32 */
-
 #endif /* _WINSOCK2_H */
